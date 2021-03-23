323a5d6c2a01f2b57429cd9ca12965c0712f0fff
Derry Pratama <derry@islab.re.kr>
2021-03-23 18:38:10 +0900

    log inst

--- debug_user_prog.sh
+++ debug_user_prog.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-./scripts/start-qemu.sh
-riscv64-unknown-linux-gnu-gdb -x script.gdb 
+rm gdb.txt
+rm result.txt
+#run gdb
+riscv64-unknown-linux-gnu-gdb -x script.gdb -batch-silent
+#parse log
+cat gdb.txt \
+| grep "=>" \
+| awk '{ for (i=1; i<=NF; ++i) { if ($i ~ ":$") print $(i+1) } }' \
+| tr ' ' '\12' | sort | uniq -c | sort -nr > result.txt
--- script.gdb
+++ script.gdb
@@ -3,9 +3,19 @@
 # hbreak start_kernel
 
 target remote :1234
+set confirm off
 
 # User program compiled with 
 # riscv64-unknown-linux-gnu-gcc -g -O0 hello.c -o hello -static
 file ./hello
-b derry
+b main
 c
+set logging overwrite on
+set logging on
+set height 0
+set style enabled off
+while (1) 
+    x/i $pc
+    stepi
+end
+quit
